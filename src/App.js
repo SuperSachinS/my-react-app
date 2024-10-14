83a81b3d891ce61e4b7fce7842360f9dd2880689
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2024-10-14 13:37:43 -0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div className="App">
+      <div className = "App">
+        <MyComponent />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MyComponent = () => {
+    return(
+        <div>
+            <h1>Hello this my First React Component</h1>
+        </div>
+    );
+};
+export default MyComponent;
