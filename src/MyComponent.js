f885f28aa2f78a4b696d4649db159e93500a7352
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2024-10-19 18:43:29 -0600

    styled mycomponent

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./MyComponent.css";
 const MyComponent = () => {
     return(
-        <div>
+        <div className ="my-component">
             <h1>Hello this my First React Component</h1>
         </div>
     );
